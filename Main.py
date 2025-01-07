4257f6f070a0ae1128e2076c044d2dec3c2876ad
Gustavo Henrique Silva de Souza <136124022+gustavohen27@users.noreply.github.com>
2025-01-06 21:31:35 -0300

    Primeiro commit do projeto

---
+++ Main.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+from tkinter import messagebox, colorchooser, filedialog
+
+import qrcode
+from PIL import Image, ImageTk, ImageColor
+from io import BytesIO
+
+def main():
+    ...
+
+print("Olá mundo!")
+
